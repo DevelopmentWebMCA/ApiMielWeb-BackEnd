2e6869db181543cc364e36805c36d84881d9c1cb
Cristhian <cristhianE2018>
2021-06-13 23:13:00 -0500

    initial commit

---
+++ src/main/java/mca/apimiel/ApimielApplication.java
@@ -0,0 +1,13 @@
+package mca.apimiel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApimielApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApimielApplication.class, args);
+	}
+
+}
---
+++ src/test/java/mca/apimiel/ApimielApplicationTests.java
@@ -0,0 +1,13 @@
+package mca.apimiel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApimielApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
